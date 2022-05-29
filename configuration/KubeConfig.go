06ceacb59f965138579f643d718c5fb5e4599b7f
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2022-05-29 21:28:30 +0100

    fix: keep static kubeconfig to prevent duplication error

--- configuration/KubeConfig.go
+++ configuration/KubeConfig.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -14,10 +15,33 @@ const (
 	K8S_NS_FILE = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
 )
 
+var (
+	config     *rest.Config
+	configLock = &sync.Mutex{}
+)
+
 func GetKubernetesConfig() (*rest.Config, error) {
 
-	if _, err := os.Stat(K8S_NS_FILE); err == nil {
-		return rest.InClusterConfig()
+	if config != nil {
+		return config, nil
+	}
+
+	// lock ensures that we only ever have one config file
+	// the lock is after the initial check as it can be lock free for most cases
+	configLock.Lock()
+	defer configLock.Unlock()
+
+	// there's a slight race condition between the first check and the lock,
+	// so check again inside a synchronised context
+	if config != nil {
+		return config, nil
+	}
+
+	var err error
+
+	if _, err = os.Stat(K8S_NS_FILE); err == nil {
+		config, err = rest.InClusterConfig()
+		return config, err
 	}
 
 	var kubeconfig *string
@@ -27,5 +51,6 @@ func GetKubernetesConfig() (*rest.Config, error) {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 
-	return clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	return config, err
 }
